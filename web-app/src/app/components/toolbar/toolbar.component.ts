f1c60d1343f1aa7a99d5a9a5f180f7ca8ca60e0a
Fábio Balbino <fbribeiro@live.com>
2023-09-10 21:26:56 -0300

    feat: menu

--- web-app/src/app/components/toolbar/toolbar.component.ts
+++ web-app/src/app/components/toolbar/toolbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { BehaviorSubject } from 'rxjs';
+import { UserRole } from 'src/app/models/user';
 import { AppState } from 'src/app/stores/app-state';
 import { settingsSelectors } from 'src/app/stores/settings/settings.selector';
 import { ToolbarActions } from './toolbar.actions';
@@ -16,6 +18,10 @@ export class ToolbarComponent {
   accessibilityMode$ = this.store.select(
     settingsSelectors.selectAccessibilityMode
   );
+
+  userRole$ = new BehaviorSubject<UserRole>(UserRole.ADMIN);
+
+  UserRole = UserRole;
 
   constructor(private readonly store: Store<AppState>) {}
 
